b1ce24374fdb9520974cb24fdf05bc2e68519afb
Jurij Retzlaff <jurij@topofweb.de>
2022-06-16 19:47:35 +0200

    fix PWM ramp matrix MOW800 motor control works

--- sunray/src/stm32/matrix_mow800.cpp
+++ sunray/src/stm32/matrix_mow800.cpp
@@ -12,10 +12,11 @@
 
 #if defined(__MOW800__)
 
-// HardwareSerial Serial1(PIN_SERIAL_RX, PIN_SERIAL_TX);
-// HardwareSerial Serial2(PIN_SERIAL2_RX, PIN_SERIAL2_TX);
-// HardwareSerial Serial3(PIN_SERIAL3_RX, PIN_SERIAL3_TX);
-HardwareSerial Serial4(PC11, PC10);
+//HardwareSerial Serial1(PIN_SERIAL_RX, PIN_SERIAL_TX);
+//HardwareSerial Serial2(PIN_SERIAL2_RX, PIN_SERIAL2_TX);
+HardwareSerial Serial4(PD6, PD5);
+//HardwareSerial Serial3(PIN_SERIAL3_RX, PIN_SERIAL3_TX);
+//HardwareSerial Serial4(PC11, PC10);
 //HardwareSerial Serial5(PD9, PD8);
  
 WatchdogSTM32 watchdog;
