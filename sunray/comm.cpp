aa72a5846da0d3cd59529f120907c7b6db06f7d4
Jurij Retzlaff <jurij@topofweb.de>
2022-06-20 00:16:24 +0200

    works on Matrix MOW800 support - gear motors works - mow motor works - charging station is detected - added max RPM param for gear motors

--- sunray/comm.cpp
+++ sunray/comm.cpp
@@ -769,10 +769,10 @@ void processCmd(bool checkCrc, bool decrypt){
             cmd[i] = char(code);  
           }
         }
-        #ifdef VERBOSE
+        //#ifdef VERBOSE
           CONSOLE.print("decrypt:");
           CONSOLE.println(cmd);
-        #endif
+        //#endif
       }
     } 
   }
--- sunray/src/driver/AmRobotDriver.h
+++ sunray/src/driver/AmRobotDriver.h
@@ -85,6 +85,7 @@ class AmMotorDriver: public MotorDriver {
     DriverChip BLDC8015A;
     DriverChip JYQD;
     DriverChip MOW800_MC33035;
+    DriverChip MOW800_MC33035_MOW;
     DriverChip CUSTOM;
     DriverChip mowDriverChip;
     DriverChip gearsDriverChip;
