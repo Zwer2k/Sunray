4298dbb787432442c4c17d3428be56bd7c6a3590
Jurij Retzlaff <jurij@topofweb.de>
2022-06-24 21:26:21 +0200

    work on Matrix MOW800 - bumper - motor controll improvement - STM32 watchdog implemented

--- sunray/robot.cpp
+++ sunray/robot.cpp
@@ -228,6 +228,8 @@ void sensorTest(){
         CONSOLE.print("\t");
       }    
       if (BUMPER_ENABLE){
+        bool leftBumper, rightBumper;
+        bumper.getTriggeredBumper(leftBumper, rightBumper);
         CONSOLE.print("bumper (triggered): ");
         CONSOLE.print(((int)bumper.obstacle()));
         CONSOLE.print("\t");
@@ -626,7 +628,7 @@ void start(){
   #ifdef ENABLE_NTRIP
     ntrip.begin();  
   #endif
-  
+
   watchdogEnable(10000L);   // 10 seconds  
   
   startIMU(false);        
